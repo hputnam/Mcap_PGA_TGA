f4732fc2558a56f59a5f57aefb4fe0e692b100e8
Hollie Putnam <hollieputnam@gmail.com>
2017-03-14 14:39:50 -0400

    updating respirometry calculations using olito et al 2017

--- RAnalysis/Scripts/Respirometry.R
+++ RAnalysis/Scripts/Respirometry.R
@@ -22,21 +22,21 @@ mainDir<-"/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/" #set main directory
 
 #PHTOTSYNTHESIS
 path.p<-"/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Data/Respirometry/Photo" #the location of all your respirometry files
-file.names<-list.files(path = path.p, pattern = "csv$")
-photo.R <- data.frame(matrix(NA, nrow=length(file.names), ncol=3, dimnames=list(file.names,c("Sample.ID","Intercept", "Slope")))) #generate a 3 column dataframe with specific column names
+file.names<-list.files(path = path.p, pattern = "csv$") #list all csv file names in the folder
+Photo.R <- data.frame(matrix(NA, nrow=length(file.names), ncol=3, dimnames=list(file.names,c("Fragment.ID","Intercept", "Slope")))) #generate a 3 column dataframe with specific column names
 
 for(i in 1:length(file.names)) { # for every file in list start at the first and run this following function
   Photo.Data <-read.table(file.path(path.p,file.names[i]), header=TRUE, sep=",", na.string="NA", as.is=TRUE, fileEncoding="latin1") #reads in the data files
-  Photo.Data  <- Photo.Data [,c(9,16)]
-  n<-dim(Photo.Data )[1]
-  Photo.Data <-Photo.Data [120:(n-3),]
-  n<-dim(Photo.Data )[1]
-  Photo.Data $sec <- 1:n
+  Photo.Data  <- Photo.Data [,c(9,16)] #subset columns of interest
+  n<-dim(Photo.Data )[1] #identify length of data
+  Photo.Data <-Photo.Data [60:(n-3),] #start at data point 1 minute in to avoid excess noise from start of run and remove last 3 lines containing text
+  n<-dim(Photo.Data )[1] #list length of trimmed data
+  Photo.Data $sec <- 1:n #set seconds by one from start to finish of run
+  #Save plot prior to and after data thinning to make sure thinning is not too extreme
   pdf("/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Output/photo.thinning.pdf")
-  par(omi=rep(0.3, 4))
-  par(mfrow=c(1,2))
-  plot(Value ~ sec, data=Photo.Data , 
-       xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE)
+  par(omi=rep(0.3, 4)) #set size of the outer margins in inches
+  par(mfrow=c(1,2)) #set number of rows and columns in multi plot graphic
+  plot(Value ~ sec, data=Photo.Data , xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE) #plot data as a function of time
   usr  <-  par('usr')
   rect(usr[1], usr[3], usr[2], usr[4], col='grey90', border=NA)
   whiteGrid()
@@ -45,9 +45,9 @@ for(i in 1:length(file.names)) { # for every file in list start at the first and
   axis(1)
   axis(2, las=1)
   
-  Photo.Data   <-  thinData(Photo.Data , by=10)$newData1
-  Photo.Data $sec <- as.numeric(rownames(Photo.Data ))
-  plot(Value ~ sec, data=Photo.Data , xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE)
+  Photo.Data   <-  thinData(Photo.Data , by=10)$newData1 #thin data by every 10 points
+  Photo.Data $sec <- as.numeric(rownames(Photo.Data )) #maintain numeric values for time
+  plot(Value ~ sec, data=Photo.Data , xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE) #plot thinned data
   usr  <-  par('usr')
   rect(usr[1], usr[3], usr[2], usr[4], col='grey90', border=NA)
   whiteGrid()
@@ -63,31 +63,31 @@ for(i in 1:length(file.names)) { # for every file in list start at the first and
   plot(Regs)
   dev.off()
   
-  photo.R[i,2:3] <- Regs$allRegs[1,c(4,5)] #inserts them in the dataframe
-  photo.R[i,1] <- substr(file.names[i],1,8) #stores the file name in the Date column
+  Photo.R[i,2:3] <- Regs$allRegs[1,c(4,5)] #inserts slope and intercept in the dataframe
+  Photo.R[i,1] <- sub("_.*", "", file.names[i]) #stores the file name in the Date column
 }
 
-photo.R$rate.min <- photo.R$Slope*60
-photo.R
+Photo.R
 
 #RESPIRATION
 
 path.r<-"/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Data/Respirometry/Resp/" #the location of all your respirometry files
 file.names.r<-list.files(path = path.r, pattern = "csv$")
-Resp.R <- data.frame(matrix(NA, nrow=length(file.names.r), ncol=3, dimnames=list(file.names.r,c("Sample.ID","Intercept", "Slope")))) #generate a 3 column dataframe with specific column names
+Resp.R <- data.frame(matrix(NA, nrow=length(file.names.r), ncol=3, dimnames=list(file.names.r,c("Fragment.ID","Intercept", "Slope")))) #generate a 3 column dataframe with specific column names
 
 for(i in 1:length(file.names.r)) { # for every file in list start at the first and run this following function
   Resp.Data <-read.table(file.path(path.r,file.names.r[i]), header=TRUE, sep=",", na.string="NA", as.is=TRUE, fileEncoding="latin1") #reads in the data files
-  Resp.Data  <- Resp.Data[,c(9,16)]
-  n<-dim(Resp.Data)[1]
-  Resp.Data <-Resp.Data[120:(n-3),]
-  n<-dim(Resp.Data)[1]
-  Resp.Data$sec <- 1:n
+  Resp.Data  <- Resp.Data[,c(9,16)] #subset columns of interest
+  n<-dim(Resp.Data)[1] #identify length of data
+  Resp.Data <-Resp.Data[60:(n-3),] #start at data point 1 minute in to avoid excess noise from start of run and remove last 3 lines containing text
+  n<-dim(Resp.Data)[1] #list length of trimmed data
+  Resp.Data$sec <- 1:n #set seconds by one from start to finish of run
+  #Save plot prior to and after data thinning to make sure thinning is not too extreme
   pdf("/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Output/resp.thinning.pdf")
-  par(omi=rep(0.3, 4))
-  par(mfrow=c(1,2))
-  plot(Value ~ sec, data=Resp.Data , 
-       xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE)
+  par(omi=rep(0.3, 4)) #set size of the outer margins in inches
+  par(mfrow=c(1,2)) #set number of rows and columns in multi plot graphic
+  plot(Value ~ sec, data=Resp.Data , xlab='Time (seconds)', ylab=substitute(' O'[2]~' (µmol/L)'), type='n', axes=FALSE) #plot data as a function of time
+  #set plot layout info
   usr  <-  par('usr')
   rect(usr[1], usr[3], usr[2], usr[4], col='grey90', border=NA)
   whiteGrid()
@@ -115,35 +115,18 @@ for(i in 1:length(file.names.r)) { # for every file in list start at the first a
   dev.off()
   
   Resp.R[i,2:3] <- Regs$allRegs[1,c(4,5)] #inserts them in the dataframe
-  Resp.R[i,1] <- substr(file.names.r[i],1,8) #stores the file name in the Date column
+  Resp.R[i,1] <- sub("_.*", "", file.names.r[i]) #stores the sample name in the Sample.ID column
 }
 
-Resp.R$rate.min <- Resp.R$Slope*60
-Resp.R
+Resp.R #view data
 
+#Load Sample Info
+Sample.Info <- read.csv(file="Respirometry_Data.csv", header=T) #read in volume and sample.info data
+Sample.Info$Vol.L <- (Sample.Info$Chamber.Vol.mL-Sample.Info$Sample.Displacement.ml)/1000 #calculate volume
 
-Flux.Info <- read.csv(file="Respirometry_Data.csv", header=T)
-Flux.Info$Vol.L <- (Flux.Info$Chamber.Vol.mL-Flux.Info$Sample.Displacement.ml)/1000
-
-# Amb.Photo.Blank
-# High.Photo.Blank
-# Amb.Resp.Blank 
-# High.Resp.Blank
-
-
-
-#ANOTHER OPTION
-#want to find photosyn local slope
-#want to find resp local slope
-#want to identify upper and lower data points and 
-#then use them to extract all the unthinned data and re-run the regression
-
-
-#Load calculated data
-data <- read.csv("/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Data/Respirometry_Data.csv", header=T, sep=",")
-
-#####RESPIRATION#####
-resp.data <- read.csv("Respirometry_Data.csv", header=TRUE, sep=",", na.strings="NA") #load data with a header, separated by commas, with NA as NA
+#Merge with sample info
+Resp <- merge(Resp.R,Sample.Info, by="Fragment.ID")
+Photo <- merge(Photo.R,Sample.Info, by="Fragment.ID")
 
 #Load Surface Area standards from surface area by foil method (Marsh JA (1970) Primary productivity of reef-building calcareous red algae. Ecology 51:255–263)
 FoilStnd<-read.csv('foil.standards.csv', header=T, sep=",") #load data
@@ -157,44 +140,81 @@ SA.Data$SA.final.cm2 <- (FoilStnd.lm$coef[2]*SA.Data$final.mass.g)+FoilStnd.lm$c
 SA.Data$SA.dead.cm2 <- (FoilStnd.lm$coef[2]*SA.Data$dead.mass.g)+FoilStnd.lm$coef[1] #calculate surface area for dead area
 SA.Data$SA.total.initial.cm2 <- (FoilStnd.lm$coef[2]*(SA.Data$dead.mass.g+SA.Data$final.mass.g))+FoilStnd.lm$coef[1] #calculate total live surface area at the start of the experiment
 
-resp.data <- merge(resp.data, SA.Data, by="Fragment.ID")
+#Merge with surface area data
+Resp <- merge(Resp, SA.Data[,c(1,6)], by="Fragment.ID")
+Photo <- merge(Photo, SA.Data[,c(1,6)], by="Fragment.ID")
 
-resp.data$Pnet_umol.min <- (resp.data$Pnet_umol.L.min*(resp.data$Chamber.Vol.mL/1000))
-resp.data$Rdark_umol.min <- (resp.data$Rdark_umol.L.min*(resp.data$Chamber.Vol.mL/1000))
-resp.data$Pnet.Blank_umol.min <- (resp.data$Blank_Pnet_umol.L.m*(resp.data$Chamber.Vol.mL/1000))
-resp.data$Rdark.Blank_umol.min <- (resp.data$Blank_Rdark_umol.L.m*(resp.data$Chamber.Vol.mL/1000))
-resp.data$Coral_Pnet_umol.min <- resp.data$Pnet_umol.min-resp.data$Pnet.Blank_umol.min
-resp.data$Coral_Rdark_umol.min <- resp.data$Rdark_umol.min-resp.data$Rdark.Blank_umol.min
-resp.data$Coral_Pnet_umol.min.cm2 <- resp.data$Coral_Pnet_umol.min/resp.data$SA.total.initial.cm2
-resp.data$Coral_Rdark_umol.min.cm2 <- resp.data$Coral_Rdark_umol.min/resp.data$SA.total.initial.cm2
-resp.data$Coral_Pnet_umol.hr.cm2 <- resp.data$Coral_Pnet_umol.min.cm2*60
-resp.data$Coral_Rdark_umol.hr.cm2 <- resp.data$Coral_Rdark_umol.min.cm2*60
-resp.data$Coral_Pgross_umol.hr.cm2 <- resp.data$Coral_Pnet_umol.hr.cm2 - resp.data$Coral_Rdark_umol.hr.cm2
+#Account for blank rate
+Amb.P.Blank <- Photo.R[which(Photo.R$Fragment.ID == "AmbientBlank1.P"),3]
+Amb.R.Blank <- Resp.R[which(Resp.R$Fragment.ID == "AmbientBlank1.R"),3]
+High.P.Blank <- Photo.R[which(Photo.R$Fragment.ID == "CO2Blank1.P"),3]
+High.R.Blank <- Resp.R[which(Resp.R$Fragment.ID == "CO2Blank1.R"),3]
+
+#Respiration
+Amb.indices <- (Resp$Treatment=="Ambient") # indices is a logical vector with TRUE and FALSE
+High.indices <- (Resp$Treatment=="High") # indices is a logical vector with TRUE and FALSE
+Resp$corr.rate <- NA
+Resp$corr.rate[Amb.indices] <- (Resp$rate.L.min - Amb.R.Blank)[Amb.indices]
+Resp$corr.rate[High.indices] <- (Resp$rate.L.min - High.R.Blank)[High.indices]
+
+#Photosynthesis
+Amb.indices <- (Photo$Treatment=="Ambient") # indices is a logical vector with TRUE and FALSE
+High.indices <- (Photo$Treatment=="High") # indices is a logical vector with TRUE and FALSE
+Photo$corr.rate <- NA
+Photo$corr.rate[Amb.indices] <- (Photo$rate.L.min - Amb.P.Blank)[Amb.indices]
+Photo$corr.rate[High.indices] <- (Photo$rate.L.min - High.P.Blank)[High.indices]
 
 
+#Convert to µmol L-1 min-1
+Photo$rate.L.min <- Photo$corr.rate*60 #converts to µmol L-1 min-1
+Resp$rate.L.min <- Resp$corr.rate*60 #converts to µmol L-1 min-1
+
+#Account for chamber size and sample displacement
+Photo$rate.min <- Photo$rate.L.min*Photo$Vol.L 
+Resp$rate.min <- Resp$rate.L.min*Resp$Vol.L 
+
+#Normalize to Surface Area
+Photo$rate <- Photo$rate.min/Photo$SA.total.initial.cm2
+Resp$rate <- Resp$rate.min/Resp$SA.total.initial.cm2
+  
+#Photo$rate <- Photo$rate*60
+#Resp$rate <- Resp$rate*60
+
+#Test against data selected by eye
+
+par(mfrow=c(1,2))
+plot(Sample.Info$Pnet_umol.L.min, Photo$rate)
+plot(Sample.Info$Rdark_umol.L.min, Resp$rate)
 
 
-Pnet.mean <- aggregate(Coral_Pnet_umol.hr.cm2 ~ History*Treatment, data=resp.data, mean)
-Pnet.se <- aggregate(Coral_Pnet_umol.hr.cm2 ~ History*Treatment, data=resp.data, std.error)
-Pnet.n <- aggregate(Coral_Pnet_umol.hr.cm2 ~ History*Treatment, data=resp.data, length)
+#calculate gross photosynthesis Pnet -- Rdark
+resp.data <- merge(Photo[,c(1,4,5,17)],Resp[,c(1,17)],  by="Fragment.ID")
+colnames(resp.data)[4] <- "Pnet_umol.cm2.hr"
+colnames(resp.data) [5] <- "Rdark_umol.cm2.hr"
+resp.data$Pgross_umol.cm2.hr <- resp.data$Pnet_umol.cm2.hr-resp.data$Rdark_umol.cm2.hr
 
-Pnet.means <- cbind(Pnet.mean, Pnet.se$Coral_Pnet_umol.hr.cm2, Pnet.n$Coral_Pnet_umol.hr.cm2) #combine mean and standard error results
+#Calculate and plot by conditions
+Pnet.mean <- aggregate(Pnet_umol.cm2.hr ~ History*Treatment, data=resp.data, mean)
+Pnet.se <- aggregate(Pnet_umol.cm2.hr ~ History*Treatment, data=resp.data, std.error)
+Pnet.n <- aggregate(Pnet_umol.cm2.hr ~ History*Treatment, data=resp.data, length)
+
+Pnet.means <- cbind(Pnet.mean, Pnet.se$Pnet_umol.cm2.hr, Pnet.n$Pnet_umol.cm2.hr) #combine mean and standard error results
 colnames(Pnet.means) <- c("History","Treatment", "mean", "se", "n")  #rename columns to describe contents
 Pnet.means$Metric <- "Net Photosynthesis"
 
-Rdark.mean <- aggregate(Coral_Rdark_umol.hr.cm2 ~ History*Treatment, data=resp.data, mean)
-Rdark.se <- aggregate(Coral_Rdark_umol.hr.cm2 ~ History*Treatment, data=resp.data, std.error)
-Rdark.n <- aggregate(Coral_Rdark_umol.hr.cm2 ~ History*Treatment, data=resp.data, length)
+Rdark.mean <- aggregate(Rdark_umol.cm2.hr ~ History*Treatment, data=resp.data, mean)
+Rdark.se <- aggregate(Rdark_umol.cm2.hr ~ History*Treatment, data=resp.data, std.error)
+Rdark.n <- aggregate(Rdark_umol.cm2.hr ~ History*Treatment, data=resp.data, length)
 
-Rdark.means <- cbind(Rdark.mean, Rdark.se$Coral_Rdark_umol.hr.cm2, Rdark.n$Coral_Rdark_umol.hr.cm2) #combine mean and standard error results
+Rdark.means <- cbind(Rdark.mean, Rdark.se$Rdark_umol.cm2.hr, Rdark.n$Rdark_umol.cm2.hr) #combine mean and standard error results
 colnames(Rdark.means) <- c("History","Treatment", "mean", "se", "n")  #rename columns to describe contents
 Rdark.means$Metric <- "Dark Respiration"
 
-Pgross.mean <- aggregate(Coral_Pgross_umol.hr.cm2 ~ History*Treatment, data=resp.data, mean)
-Pgross.se <- aggregate(Coral_Pgross_umol.hr.cm2 ~ History*Treatment, data=resp.data, std.error)
-Pgross.n <- aggregate(Coral_Pgross_umol.hr.cm2 ~ History*Treatment, data=resp.data, length)
+Pgross.mean <- aggregate(Pgross_umol.cm2.hr ~ History*Treatment, data=resp.data, mean)
+Pgross.se <- aggregate(Pgross_umol.cm2.hr ~ History*Treatment, data=resp.data, std.error)
+Pgross.n <- aggregate(Pgross_umol.cm2.hr ~ History*Treatment, data=resp.data, length)
 
-Pgross.means <- cbind(Pgross.mean, Pgross.se$Coral_Pgross_umol.hr.cm2, Pgross.n$Coral_Pgross_umol.hr.cm2) #combine mean and standard error results
+Pgross.means <- cbind(Pgross.mean, Pgross.se$Pgross_umol.cm2.hr, Pgross.n$Pgross_umol.cm2.hr) #combine mean and standard error results
 colnames(Pgross.means) <- c("History","Treatment", "mean", "se", "n")  #rename columns to describe contents
 Pgross.means$Metric <- "Gross Photosynthesis"
 
@@ -269,7 +289,7 @@ Fig.Rd <-  ggplot(Rdark.means, aes(x=Treatment, y=mean,  group=History)) + #set 
 Fig.Rd #view plot
 
 dev.off()
-Pn.lm <- lm(Coral_Pnet_umol.hr.cm2 ~ History*Treatment, data=resp.data)
+Pn.lm <- lm(Pnet_umol.cm2.hr ~ History*Treatment, data=resp.data)
 Pn.res <- anova(Pn.lm)
 Pn.res 
 par(mfrow=c(1,3))
@@ -277,7 +297,7 @@ hist(Pn.lm$residuals)
 boxplot(Pn.lm$residuals)
 qqplot(Pn.lm$fitted.values, Pn.lm$residuals)
 
-Pg.lm <- lm(Coral_Pgross_umol.hr.cm2 ~ History*Treatment, data=resp.data)
+Pg.lm <- lm(Pgross_umol.cm2.hr ~ History*Treatment, data=resp.data)
 Pg.res <- anova(Pg.lm)
 Pg.res
 par(mfrow=c(1,3))
@@ -285,7 +305,7 @@ hist(Pg.lm$residuals)
 boxplot(Pg.lm$residuals)
 qqplot(Pg.lm$fitted.values, Pg.lm$residuals)
 
-Rd.lm <- lm(Coral_Rdark_umol.hr.cm2 ~ History*Treatment, data=resp.data)
+Rd.lm <- lm(Rdark_umol.cm2.hr ~ History*Treatment, data=resp.data)
 Rd.res <- anova(Rd.lm)
 Rd.res
 par(mfrow=c(1,3))
@@ -294,8 +314,8 @@ boxplot(Rd.lm$residuals)
 qqplot(Rd.lm$fitted.values, Rd.lm$residuals)
 
 setwd("/Users/hputnam/MyProjects/Mcap_PGA_TGA/RAnalysis/Output/") #set working
-Resp <- arrangeGrob(Fig.Pn, Fig.Rd, Fig.Pg, ncol=3)
-ggsave(file="Respirometry.pdf", Resp, width = 11, height = 6, units = c("in"))
+Resp.Figs <- arrangeGrob(Fig.Pn, Fig.Rd, Fig.Pg, ncol=3)
+ggsave(file="Respirometry.pdf", Resp.Figs, width = 11, height = 6, units = c("in"))
 
 
 
